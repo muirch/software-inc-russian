a891bde19039b808b38149e7c500e0d9a34828e7
Ivan Muir <mimuir@ya.ru>
2026-04-11 14:13:53 +0300

    fix: исправлен deploy.bat (пробелы в пути и скобки в x86)

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,20 +1,20 @@
 @echo off
-setlocal
 
 set "TARGET=C:\Program Files (x86)\Steam\steamapps\workshop\content\362620\649847804"
 set "REPO=%~dp0.."
 
-if not exist "%TARGET%" (
-    echo Workshop directory not found at %TARGET%
-    echo Make sure you have the Russian localization subscribed in Steam Workshop.
-    pause
-    exit /b 1
-)
+if not exist "%TARGET%" goto notfound
 
 echo Copying translation files to workshop directory...
-for %%f in ("%REPO%\*.tyd") do copy /y "%%f" "%TARGET%\"
+xcopy /y "%REPO%\*.tyd" "%TARGET%\"
 copy /y "%REPO%\TypeInfo.txt" "%TARGET%\"
 copy /y "%REPO%\Thumbnail.png" "%TARGET%\"
-
 echo Done!
 pause
+goto :eof
+
+:notfound
+echo Workshop directory not found at %TARGET%
+echo Make sure you have the Russian localization subscribed in Steam Workshop.
+pause
+exit /b 1
